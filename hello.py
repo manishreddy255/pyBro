e1b0afdde6de05e63790060d64df746f2ebefb7b
manishreddy <manishreddy255@gmail.com>
2021-10-23 22:55:12 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,34 @@
+
+
+# now This is python and we will start
+
+# now we have to print hello
+
+# print("hello")
+
+
+# variable = a container for a value. Behaves as a container for some
+#     value
+
+# name = "Bro"
+# print("hello" + name)
+# print(type(name))
+
+# you can join variable as long as they are of same data type
+
+# name = "manish"
+# listing = "hey this is listing"
+# print(name + listing)
+# you can do this
+
+# now we will use another one with other data type
+# dating = 78
+# print(name + dating)
+# you can't do this
+
+# you can't concatenate two things of differeent type in python
+# you can only do it after changing the type of the variable
+# with type casting
+age = 34
+print("hey how are you my age is :" + str(age))
+
