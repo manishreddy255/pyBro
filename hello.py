c28e98772dd7943be9d437e40f74c783d2418ce6
manishreddy <manishreddy255@gmail.com>
2021-10-24 09:38:34 +0530

    some minor ops

--- hello.py
+++ hello.py
@@ -29,6 +29,100 @@
 # you can't concatenate two things of differeent type in python
 # you can only do it after changing the type of the variable
 # with type casting
-age = 34
-print("hey how are you my age is :" + str(age))
+# in this the data of one type is changed to another type
+# age = 34
+# print("hey how are you my age is :" + str(age))
+
+#This is what you call a float data type
+
+# height = 237.79
+# print(height)
+# print(type(height))
+# print("your height is : " + str(height))
+
+#boolean data type
+
+# human = True
+# animal = False
+#
+# print(animal)
+# print(type(animal))
+# print("Ar eyou a human ?: " + str(animal))
+
+#multiple assignment in python
+# assigning multiple variables at a tim
+
+# name = "manish"
+# age = 25
+# attractive = True
+# print(name)
+# print(attractive)
+# print(age)
+
+# name, age, attractive = "manish", 78, True
+#
+# print(name, age, attractive)
+#
+# spongeBob = hermit = bunny = 78
+#
+# print(spongeBob)
+# print(hermit)
+# print(bunny)
+
+#useful methods for strings
+
+name = 'manish'
+
+# print(len(name))
+# print(name.find("m"))
+# print(name.capitalize()) # emits copy with capitalized string
+# print(name.upper())
+# print(name.lower())
+# print(name.isdigit()) it can be used to know if the thing is a string or not
+# print(name.isalpha()) if the string contain all strings
+# print(name.count("a"))
+# print(name.replace('i', 'ki'))
+# print(name*78) with this you can print it multiple times
+
+#********** Type casting ****************
+# it is ability to convert one data type to another
+# x = 1
+# y = 2.0
+# z = "ama"
+# k = "89"
+#
+# # int(y) # this just emits something and doesn't change anything
+# y = int(y)
+# print(x, y, z*3)
+# k = int(k)
+# print(k*3)
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# ************ input accepting and performing op's **************
+
+# doing = input("what are you doing ?")
+# print("so you are " + doing)
+#
+# #important point whenever you give something in input
+# # it is always a string and you can only type cast it to something
+# #else
+#
+# age = int(input("hey what is your age ?: "))
+# print(age, type(age))
+# print(" So your age is " + str(age) + " then you can  who ever you want")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
